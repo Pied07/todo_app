6836521217c69cb50213dd51a1c6241636380a9c
Sandipan Adhikary <87486032+Pied07@users.noreply.github.com>
2024-09-30 10:30:08 +0530

    Update settings.py

--- TODO/settings.py
+++ TODO/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'TODO.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'todo/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'TODO/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
